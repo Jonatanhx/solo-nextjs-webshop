b0dc93ff845cd6e6204d091d1ea6ec70cfe3c283
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-06-09 20:56:52 +0200

    Added tailwind rules and styling to category cards

--- src/components/CategoryGrid.tsx
+++ src/components/CategoryGrid.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import db from "../../src/lib/db";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 
 export default async function CategoryGrid() {
   const categories = await db.category.findMany({});
   return (
-    <section>
+    <section className=" flex flex-row">
       {categories.map((category) => (
-        <Card key={category.id}>
-          <CardHeader>
+        <Card
+          key={category.id}
+          className="bg-neutral-600 text-white mx-2 max-w-100 rounded-xl border-black overflow-hidden transition ease-in-out delay-10 hover:-translate-y-1 hover:-translate-x-1 hover:ring-4 duration-300"
+        >
+          <CardHeader className="flex items-center">
             <CardTitle>{category.name}</CardTitle>
           </CardHeader>
-          <CardContent>
-            <Image
-              src={category.image}
-              alt="Various tools for sale according to title name"
-              width={250}
-              height={250}
-            ></Image>
-          </CardContent>
+          <Image
+            src={category.image}
+            alt="Various tools for sale according to title name"
+            width={600}
+            height={600}
+          ></Image>
         </Card>
       ))}
     </section>
